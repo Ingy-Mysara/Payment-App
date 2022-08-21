1c842a42239f91b45f7edb3ac263426e748bcd2e
Ingy-Mysara <67437146+Ingy-Mysara@users.noreply.github.com>
2022-08-21 12:10:59 +0200

    merged changes

--- LinearSearch.h
+++ LinearSearch.h
@@ -1,5 +1,5 @@
 #include "Card/card.h"
-#include"Server\server.h"
+#include "Server/server.h"
 
 int linearSearch(ST_accountsDB_t accountsDB[], int size, uint8_t target) {
     for (int i = 0; i < size; i++) {
