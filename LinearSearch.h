2eb7634c5b5f535fa1d2fe6a56d218583692fb81
zakoshgo <81923383+zakoshgo@users.noreply.github.com>
2022-08-21 12:01:33 +0200

    (ziad) first function update
    (ziad) first function update

---
+++ LinearSearch.h
@@ -0,0 +1,12 @@
+#include "Card/card.h"
+#include"Server\server.h"
+
+int linearSearch(ST_accountsDB_t accountsDB[], int size, uint8_t target) {
+    for (int i = 0; i < size; i++) {
+        // check whether the two strings are equal or not
+        if (strcmp((char*)accountsDB[i].primaryAccountNumber, (char*)target) == 0)
+            // if found return index of the customer
+            return i;
+    }
+    return -1;
+}
--- Server/server.c
+++ Server/server.c
@@ -1,8 +1,46 @@
 #include "server.h"
+#include "../LinearSearch.h"
 
+
+EN_transState_t transState;
 EN_transState_t receiveTransactionData(ST_transaction_t* transData)
 {
-	return;
+	// should validate the data 
+	
+	
+	uint8_t targetAccountNumber = transData->cardHolderData.primaryAccountNumber;
+	int arraySize = (sizeof(accountsDB) / sizeof(accountsDB[0]));
+	
+	
+	
+	int accountIndex = linearSearch(accountsDB, arraySize, targetAccountNumber);
+	if (accountIndex == -1) {
+		transState = FRAUD_CARD;
+		printf("card not found \n");
+		return transState;
+	}
+	// at this point i have account endex
+	int neededAmount = transData->terminalData.transAmount;
+
+	ST_accountsDB_t targetAccount = accountsDB[accountIndex];
+	float availableAmount = targetAccount.balance;
+
+	if (availableAmount < neededAmount) {
+		printf("avaliable amount is less than needed amount \n");
+		transState = DECLINED_INSUFFICIENT_FUND;
+		return transState;
+	}
+	
+	if (targetAccount.state == BLOCKED) {//// check Blocked or 1
+		transState = DECLINED_STOLEN_CARD;
+		printf("911 is on their way \n");
+		return transState;
+	}
+
+	//missing if condidion depending on save transaction function
+
+	transState = APPROVED;
+	return transState;
 }
 
 EN_serverError_t isValidAccount(ST_cardData_t cardData, ST_accountsDB_t accountRefrence)
