3a691dac290f91930247375ac7a30995c61df7c8
Bfs and Dfs <dmeckawy@gmail.com>
2022-08-22 09:02:45 +0200

    Terminal files

--- Terminal/terminal.h
+++ Terminal/terminal.h
@@ -9,14 +9,14 @@ typedef unsigned int uint32_t;
 
 typedef struct ST_terminalData_t
 {
-    float transAmount;
-    float maxTransAmount;
-    uint8_t transactionDate[11];
+    float transAmount; //Transfer amount
+    float maxTransAmount; //Maximum transfer amount
+    uint8_t transactionDate[10]; //10 characters for the date
 }ST_terminalData_t;
 
 typedef enum En_terminalError_t
 {
-    TERMINAL_OK, WRONG_DATE, EXPIRED_CARD, INVALID_CARD, INVALID_DATE, EXCEED_MAX_AMOUNT, INVALID_MAX_AMOUNT
+    TERMINAL_OK, WRONG_DATE, EXPIRED_CARD, INVALID_CARD, INVALID_AMOUNT, EXCEED_MAX_AMOUNT, INVALID_MAX_AMOUNT
 }En_terminalError_t;
 
 En_terminalError_t getTransactionDate(ST_terminalData_t* termData);
